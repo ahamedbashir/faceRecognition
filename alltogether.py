9d284ac0870a3f43187cbee7727204ca4a403efc
uchajk <ucha@localhost.localdomain>
2019-02-13 17:22:25 -0500

    something'

--- alltogether.py
+++ alltogether.py
@@ -8,32 +8,48 @@ import pickle
 
 vs = VideoStream(src=0).start()
 data = pickle.loads(open("encodings.pickle", "rb").read())
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 display_flag=False
 recognize_flag=False
+train_flag=False
 
 # Actions
-def recognize_action():
-    global display_flag
-    global recognize_flag
-    display_flag = False
-    if not recognize_flag:
-        recognize_flag = True
-        recognize()
-
 def display_action():
     global display_flag
     global recognize_flag
+    global train_flag
     recognize_flag = False
+    train_flag = False
     if not display_flag:
         display_flag = True
         display()
-    
+
+def recognize_action():
+    global display_flag
+    global recognize_flag
+    global train_flag
+    display_flag = False
+    train_flag = False
+    if not recognize_flag:
+        recognize_flag = True
+        recognize()
+        
+def train_action():
+    global display_flag
+    global recognize_flag
+    global train_flag
+    display_flag = False
+    recognize_flag = False
+    if not train_flag:
+        train_flag = True
+        train()
+        
 # Repetitive functions
 def display():
     if display_flag:
         frame = vs.read()
-        frame = imutils.resize(frame, width=700)
+        frame = imutils.resize(frame, width=400)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = Image.fromarray(frame)
         frame = ImageTk.PhotoImage(image=frame)
@@ -44,7 +60,7 @@ def display():
 def recognize():
     if recognize_flag:
         frame = vs.read()
-        frame = imutils.resize(frame, width=700)
+        frame = imutils.resize(frame, width=400)
         rects = face_recognition.face_locations(frame, model="hog")
         encodings = face_recognition.face_encodings(frame, rects)
         names = []
@@ -70,6 +86,20 @@ def recognize():
         top_container.configure(image=frame)
         top_container.after(10, recognize)
 
+def train():
+    if train_flag:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        rects = detector.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY), scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
+        for(x, y, w, h) in rects:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = Image.fromarray(frame)
+        frame = ImageTk.PhotoImage(image=frame)
+        top_container.itk  = frame
+        top_container.configure(image=frame)
+        top_container.after(10, train)
+    
 # Main Function
 root = tk.Tk()
 root.attributes("-type","dialog")
@@ -77,7 +107,7 @@ top_container = tk.Label(root, bg="grey")
 bottom_container = tk.Label(root, bg="black")
 button_stream = tk.Button(bottom_container, text="Stream", command=display_action)
 button_recognize = tk.Button(bottom_container, text="Recognize", command=recognize_action)
-button_train = tk.Button(bottom_container, text="Train")
+button_train = tk.Button(bottom_container, text="Train", command=train_action)
 button_stream.pack(side="left", padx=10)
 button_recognize.pack(side="left", padx=10)
 button_train.pack(side="left", padx=10)
